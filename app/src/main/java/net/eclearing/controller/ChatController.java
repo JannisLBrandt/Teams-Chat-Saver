9ad82e9030298371af1f749150393ae49385be74
Brandt <Brandt@smartlab-gmbh.de>
2025-09-25 15:18:14 +0200

    Added a controller to connect UI and Services. Updted placeholder main.

---
+++ app/src/main/java/net/eclearing/controller/ChatController.java
@@ -0,0 +1,36 @@
+package net.eclearing.controller;
+
+import net.eclearing.services.ChatApi;
+import java.net.URI;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import java.io.IOException;
+
+public class ChatController {
+    
+    public ChatApi chatApi;
+
+    public ChatController() {}
+
+    public ChatController(ChatApi chatApi) {
+	this.chatApi = chatApi;
+    }
+
+    public void placeHolder(String link) {
+	
+	try {
+	    JsonArray messages = this.chatApi.getMessagesAsArray();
+	    for (JsonElement e : messages) {
+		JsonObject msg = ChatApi.convertJsonElementToJsonObject(e);
+		String sender = ChatApi.getSenderName(msg);
+		String content = ChatApi.getContent(msg);
+
+		System.out.println(sender + ": " + content);//oder fülle label oder so
+	    }
+	} catch (IOException | InterruptedException ex) {
+	    ex.printStackTrace();
+	}
+    }
+    
+}
--- app/src/main/java/net/eclearing/ui/App.java
+++ app/src/main/java/net/eclearing/ui/App.java
@@ -1,7 +1,21 @@
 package net.eclearing.ui;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import java.io.IOException;
+import java.net.URI;
+
+import net.eclearing.controller.ChatController;
+import net.eclearing.services.ChatApi;
+
 public class App {
     public static void main(String[] args) {
 	System.out.println("Welcome to the main screen!");
+
+	String link = "http://localhost:8181/v1.0/chats/19:devteam-2025@thread.v2/messages";
+	ChatApi api = new ChatApi(link);
+	ChatController controller = new ChatController(api);
+	controller.placeHolder(link);
     }
 }
